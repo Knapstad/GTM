53cb62ab80c6897d65d739c97c6c04b5f242a3f8
Bendik <knaben@OBOS.NO>
2019-10-21 10:10:51 +0200

    rename posisjon.js

---
+++ Posisjon/posisjon-text.js
@@ -0,0 +1,6 @@
+function() {
+    var list = document.querySelectorAll('h3.CardItem-heading a'), // Make sure this CSS selector matches the one in the trigger
+        el   = {{Click Element}};
+    return [].indexOf.call(list, el) + 1;
+  }
+
