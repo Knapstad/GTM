d4aeb35bbbf246838337174cbeffa00144806370
Bendik <knaben@OBOS.NO>
2019-10-21 11:29:52 +0200

    change , to ;

--- Posisjon/meglernavn-fra-bilde.js
+++ Posisjon/meglernavn-fra-bilde.js
@@ -1,4 +1,4 @@
-function meglernavn_fra_bilde() {	
+function meglernavn_fra_bilde() {
     var el = {{Click Element}};
     var navn = el.parentElement.nextElementSibling.querySelector("h3").innerText
     return navn
--- Posisjon/posisjon-bilde.js
+++ Posisjon/posisjon-bilde.js
@@ -1,5 +1,5 @@
-function posisjon-bilde() {
-    var list = document.querySelectorAll('div.CardItem-image a'), // Make sure this CSS selector matches the one in the trigger
+function posisjon_bilde(){
+    var list = document.querySelectorAll('div.CardItem-image a');
         el   = {{Click Element}};
     return [].indexOf.call(list, el) + 1;
   }
