561fe013bf81700a2e5d481d74b565250fd303a2
Bendik <knaben@OBOS.NO>
2019-10-21 11:28:42 +0200

    remove comment

--- Posisjon/posisjon-text.js
+++ Posisjon/posisjon-text.js
@@ -1,5 +1,5 @@
 function posisjon_text(){
-    var list = document.querySelectorAll('h3.CardItem-heading a'), // Make sure this CSS selector matches the one in the trigger
+    var list = document.querySelectorAll('h3.CardItem-heading a');
         el   = {{Click Element}};
     return [].indexOf.call(list, el) + 1;
   }
