d80ae5c92f61ae1ade9e126589402f625eb03790
Bendik <knaben@OBOS.NO>
2019-10-21 11:27:16 +0200

    change - to _

--- Posisjon/meglernavn-fra-bilde.js
+++ Posisjon/meglernavn-fra-bilde.js
@@ -1,4 +1,4 @@
-function meglernavn-fra-bilde() {	
+function meglernavn_fra_bilde() {	
     var el = {{Click Element}};
     var navn = el.parentElement.nextElementSibling.querySelector("h3").innerText
     return navn
--- Posisjon/posisjon-text.js
+++ Posisjon/posisjon-text.js
@@ -1,4 +1,4 @@
-function posisjon-text() {
+function posisjon_text(){
     var list = document.querySelectorAll('h3.CardItem-heading a'), // Make sure this CSS selector matches the one in the trigger
         el   = {{Click Element}};
     return [].indexOf.call(list, el) + 1;
