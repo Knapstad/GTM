b2e6979b2992aa6dcf25c312ff66988c05034477
Bendik <knaben@OBOS.NO>
2019-10-21 11:38:51 +0200

    added ; to end of statemenet

--- Posisjon/meglernavn-fra-bilde.js
+++ Posisjon/meglernavn-fra-bilde.js
@@ -1,5 +1,5 @@
 function meglernavn_fra_bilde() {
     var el = {{Click Element}};
-    var navn = el.parentElement.nextElementSibling.querySelector("h3").innerText
+    var navn = el.parentElement.nextElementSibling.querySelector("h3").innerText;
     return navn
 }
