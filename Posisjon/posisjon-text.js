d86f4885d4871e1ebc5c9b91aa91a244d5cd1b75
Bendik <knaben@OBOS.NO>
2019-10-22 10:07:19 +0200

    sll test

--- Posisjon/posisjon-text.js
+++ Posisjon/posisjon-text.js
@@ -2,5 +2,6 @@ function posisjon_text(){
     var list = document.querySelectorAll('h3.CardItem-heading a');
         el   = {{Click Element}};
     return [].indexOf.call(list, el) + 1;
-  }
+  
+}
 
