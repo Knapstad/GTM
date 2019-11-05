a26355bcae86c0b2144597ea0c096bb92b68e6b7
Bendik <knaben@OBOS.NO>
2019-11-05 13:08:02 +0100

    added 100 appartment constraint

--- Besøkteleiligheter/Besokteleiligheter.js
+++ Besøkteleiligheter/Besokteleiligheter.js
@@ -1,14 +1,18 @@
+
 function getCookie(name) {
     var value = "; " + document.cookie;
     var parts = value.split("; " + name + "=");
         if (parts.length == 2) return parts.pop().split(";").shift();
 }
-(function(){
-    var currentvalue = getCookie("besokteleiligheter");
-    if(!currentvalue){
-            currentvalue="12365478"};
-    var leilighetsnummer = 222123111;
-    if(!currentvalue.split(",").includes(leilighetsnummer.toString())){
-        var newvalue = currentvalue+","+leilighetsnummer;}
-    else{newvalue = currentvalue;}
-    document.cookie="besokteleiligheter="+newvalue;})
+
+var currentvalue = getCookie("besokteleiligheter");
+var leilighetsnummer = {{DLV - Prosjektnummer}};
+if(!currentvalue){
+currentvalue=leilighetsnummer.toString();}
+if(currentvalue.length >= 900){
+currentvalue.pop()};
+if(!currentvalue.split(",").includes(leilighetsnummer.toString())){
+var newvalue = currentvalue+","+leilighetsnummer;}
+else{newvalue = currentvalue;}
+document.cookie="besokteleiligheter="+newvalue;
+
